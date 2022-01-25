ebd7d95060828b0f8ddb27cb3a2281822d3dc6dd
zxmios <980805023@qq.com>
2022-01-25 10:14:52 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by MAX on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
